ac4296d981f1ced2cdbaa5260b76469223eff09e
Himalayan <hdnpress@gmail.com>
2023-02-16 14:27:56 +0545

    An unique Tag(s) that may be associated with Post(s) or video(s) and seed.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
+use App\Models\Video;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Tag::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Post::factory(10)->create()->each(function ($post) {
+            $post->tags()->attach([rand(1, 10), rand(1, 10)]);
+        });
+
+        Video::factory(10)->create()->each(function ($video) {
+            $video->tags()->attach([rand(1, 10), rand(1, 10)]);
+        });
     }
 }
